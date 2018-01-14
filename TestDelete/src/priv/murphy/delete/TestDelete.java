0e0859e3e3993bf9261fffa1847f9276872cca30
Murphy1908 <gz_zzhe@outlook.com>
2018-01-14 16:25:23 +0800

    用于测试工程删除

---
+++ TestDelete/src/priv/murphy/delete/TestDelete.java
@@ -0,0 +1,10 @@
+package priv.murphy.delete;
+
+public class TestDelete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("delete project");
+	}
+
+}
